5d4fff58a99c0645099343e3ec1707f4d87a3feb
zeynepnur-tuncel <17050111011@ybu.edu.tr>
2022-12-07 15:59:59 +0300

    Ödev1

---
+++ ÖDEV1.sql
@@ -0,0 +1,14 @@
+--SELECT title,description FROM film;
+
+--SELECT * FROM film
+--where length > 60 and length < 75;
+
+--select * from film
+--where rental_rate = 0.99 and replacement_cost = 12.99 or replacement_cost = 28.99; 
+
+--LAST NAME IS SMITH
+--select first_name,last_name from customer
+--where first_name = 'Mary';
+
+--select * from film
+--where not length > 50 and not (rental_rate = 2.99 or rental_rate = 4.99);
