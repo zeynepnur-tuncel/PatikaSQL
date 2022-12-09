0d013d95aceda0c75156a152d8f30eaf383c9638
zeynepnur-tuncel <17050111011@ybu.edu.tr>
2022-12-09 16:37:07 +0300

    Create Ödev6.sql

---
+++ Ödev6.sql
@@ -0,0 +1,15 @@
+
+--2.980
+--SELECT ROUND (AVG(rental_rate),3) FROM film;
+
+--92
+/*SELECT COUNT(*) FROM film
+WHERE title LIKE 'C%';*/
+
+--184
+/*SELECT MAX(length) FROM film
+WHERE rental_rate = 0.99;*/
+
+--21
+/*SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length >150;*/
