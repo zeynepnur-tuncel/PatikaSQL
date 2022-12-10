95fafe68922c8fc6cf80ef0b4ac7f65a29d810d8
zeynepnur-tuncel <17050111011@ybu.edu.tr>
2022-12-10 21:29:31 +0300

    Create Ödev10.sql

---
+++ Ödev10.sql
@@ -0,0 +1,17 @@
+--1.soru
+/*SELECT city,country
+FROM city
+LEFT JOIN country ON city.city_id = country.country_id;*/
+
+--2.soru
+/*SELECT payment_id,first_name,last_name
+FROM payment
+RIGHT JOIN customer ON customer.store_id = payment.staff_id;*/
+
+--3.soru
+/*SELECT rental_id,first_name,last_name
+FROM customer
+FULL JOIN rental ON  rental.customer_id=customer.customer_id;*/
+
+
+
